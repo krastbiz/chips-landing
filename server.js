a2ad2d03e4e58df8d7e1384aa25049eb43051639
krastbiz <Alexander.turchin@corcoran.com>
2023-03-28 18:01:21 +0300

    refactor server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const next = require('next')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const HOSTNAME = 'localhost'
-const PORT = 3000
+const HOSTNAME = process.env.SITE_BASE_URL
+const PORT = process.env.PORT
+
 const devArgStr = process.argv.slice(-1)[0]
 const isDevMode = devArgStr.split('=')[1] === 'true'
 
@@ -19,7 +22,11 @@ app.prepare().then(() => {
     const serverListening = server.listen(PORT, (error) => {
         if (error) console.error("[Server] ERROR: ", error)
 
+        console.log('=================================')
         console.log('[Server] App is running!')
+        console.log(`[Server] Mode: ${isDevMode ? 'DEVELOPMENT' : 'PRODUCTION'}`)
+        console.log(`[Server] URL: http://${HOSTNAME}:${PORT}`)
+        console.log('=================================')
     })
 
     return serverListening
