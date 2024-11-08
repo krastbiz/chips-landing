f98e03710f551904c92d756702314eef36fdadf4
Sergey Zmitrukevich <sergeyzmitrukevich@yandex.ru>
2024-11-08 03:27:59 +0300

    update yandex metrika

--- pages/_document.js
+++ pages/_document.js
@@ -42,27 +42,19 @@ export default class MyDocument extends Document {
                         type="text/javascript"
                         dangerouslySetInnerHTML={{
                             __html: `
-        (function(m,e,t,r,i,k,a){
-          m[i]=m[i]||function(){
-            (m[i].a=m[i].a||[]).push(arguments)
-          };
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {
-            if (document.scripts[j].src === r) { return; }
-          }
-          k=e.createElement(t),
-          a=e.getElementsByTagName(t)[0],
-          k.async=1,k.src=r,
-          a.parentNode.insertBefore(k,a)
-        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(98174314, "init", {
-          clickmap: true,
-          trackLinks: true,
-          accurateTrackBounce: true,
-          webvisor: true
-        });
-      `,
+                ym(98566823, "init", {
+                  clickmap: true,
+                  trackLinks: true,
+                  accurateTrackBounce: true,
+                  webvisor: true
+                });
+              `,
                         }}
                     />
 
@@ -70,7 +62,7 @@ export default class MyDocument extends Document {
                     <noscript>
                         <div>
                             <img
-                                src="https://mc.yandex.ru/watch/98174314"
+                                src="https://mc.yandex.ru/watch/98566823"
                                 style={{ position: 'absolute', left: '-9999px' }}
                                 alt=""
                             />
